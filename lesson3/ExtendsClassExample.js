9671924a10005f9fdc3bcc1a43a7c8793340866a
tranductri138 <tritocao@gmail.com>
2021-09-08 04:13:16 +0700

    four commit

--- lesson3/ExtendsClassExample.js
+++ lesson3/ExtendsClassExample.js
@@ -21,3 +21,12 @@ class DateFormatter extends Date {
 
 console.log(new DateFormatter("August 19, 1975 23:15:30").getFormattedDate());
 // expected output: "19-Aug-1975"
+
+// Ly thuyet 1 chut
+// this is syntax
+// class ChildClass extends ParentClass { ... }
+// Description
+// the extends is key word can be used to subclass
+// if .prototype must be an Object or null not undefine
+
+

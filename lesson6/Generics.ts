e59e15a6bd0d9a152fc7c87c459045d31425e2c3
tranductri138 <tritocao@gmail.com>
2021-09-13 09:57:14 +0700

    9h57

---
+++ lesson6/Generics.ts
@@ -0,0 +1,9 @@
+// Generics là các thức lập trình tổng quát cho phép một obj hoạt động với nhiều 
+// kiểu dữ liệu khác nhau 
+//=============================
+// Thuật ngữ "Generics " nghĩa là tham số hóa kiểu dữ liệu . Tham số hóa kiểu dữ liệu 
+// rất quan trọng vì nó cho phép chúng ta tạo ra và sử dụng một classs, interface  với 
+// nhiều kiểu dữ liệu khác nhau 
+//=============================
+// Một class , interface hay một method mà thực hiện trên một kiểu tham số xác định
+// thì sẽ gọi là generic

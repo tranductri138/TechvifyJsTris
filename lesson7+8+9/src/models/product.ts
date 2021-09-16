cefd9566ad31dec78f9a5826e5774fdf02a6a50b
tranductri138 <tritocao@gmail.com>
2021-09-16 08:54:11 +0700

    first commit 16 thang 9

---
+++ lesson7+8+9/src/models/product.ts
@@ -0,0 +1,49 @@
+class Product {
+    private id: number;
+    private name: string
+    private price: number;
+    private description: string
+    constructor(
+        id: number,
+        name: string,
+        price: number,
+        description: string
+    ) {
+        this.id = id
+        this.name = name
+        this.price = price
+        this.description = description
+    }
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public getPrice(): number {
+        return this.price;
+    }
+
+    public setPrice(price: number): void {
+        this.price = price;
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public setDescription(description: string): void {
+        this.description = description;
+    }
+}
+export { Product}
